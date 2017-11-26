e5aacd825d940b792824cfbf028de70800dd0212
aaverkova <32583192+aaverkova@users.noreply.github.com>
2017-11-27 00:30:33 +0100

    Delete Calculator.java

--- src/averkova_ebner/Calculator.java
+++
@@ -1,6 +0,0 @@
-package averkova_ebner;
-
-public interface Calculator {
-   /** returns the result or throws an (Runtime-) Exception if anything goes wrong (e.g. illegal input) */
-   public double calc(String[] input);
-}
