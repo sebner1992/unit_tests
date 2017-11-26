cb8e71c0a979e7163feaa3c23a3b33182a4bc101
aaverkova <32583192+aaverkova@users.noreply.github.com>
2017-11-27 00:24:46 +0100

    Add files via upload

---
+++ test/averkova_ebner/RpnCalculatorTests.java
@@ -0,0 +1,96 @@
+package averkova_ebner;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RpnCalculatorTests {
+	private RpnCalculator rpn;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		rpn = new RpnCalculator();
+		StackLinkedList<String> stackLinkedList = new StackLinkedList<String>(10);
+		rpn.setStack(stackLinkedList);
+	}
+
+	@Test
+	public void testArithmeticOperations() {
+		CalculatorIO[] io = {new CalculatorIO (68.0, new String[]{"70", "2", "-"}),
+				new CalculatorIO (150.0, new String[]{"10", "5", "3", "*", "*"}),
+				new CalculatorIO (1.66, new String[]{"10", "6", "/"}),
+				new CalculatorIO (39.0, new String[]{"30", "4", "5", "+", "+"})};  
+
+		for(CalculatorIO item : io) {
+			assertEquals(item.result, rpn.calc(item.input), 0.01);
+		}		
+	}
+
+	@Test
+	public void divisionByZeroShouldThrowException() {
+		String[] input = {"0", "0", "/"};
+		Throwable exception = Assertions.assertThrows(ArithmeticException.class,
+				() -> {
+					rpn.calc(input);
+				});
+		assertEquals("ERROR! Division by zero! Program terminated.", 
+				exception.getMessage());
+	}
+
+	@Test
+	public void oneNullOrEmptyArgumentShouldThrowException() {
+		String[][] input = {{null, "0", "/"}, {"", "0", "/"}};
+
+		for(String[] inp : input) {
+			Throwable exception = Assertions.assertThrows(IllegalArgumentException.class,
+					() -> {
+						rpn.calc(inp);
+					});
+			assertEquals("ERROR! The argument"+"\""+inp[0]+"\" is null or empty.", 
+					exception.getMessage());
+		}
+	}
+
+	@Test
+	public void emptyArgumentsShouldThrowException() {
+		String[] input = {};
+		Throwable exception = Assertions.assertThrows(RuntimeException.class,
+				() -> {
+					rpn.calc(input);
+				});
+		assertEquals("ERROR! No arguments.", exception.getMessage());
+	}
+
+	@Test
+	public void invalidInputShouldThrowException() {
+		String[] input = {"1", "2", "bb"};
+		Throwable exception = Assertions.assertThrows(RuntimeException.class,
+				() -> {
+					rpn.calc(input);
+				});
+		assertEquals("ERROR! Invalid operator: " + input[2], exception.getMessage());
+	}
+
+	@Test
+	public void ThrowExceptionIfOperandCantBeParsedToDouble() {
+		String[] input = {"1b", "2", "+"};
+		Throwable exception = Assertions.assertThrows(RuntimeException.class,
+				() -> {
+					rpn.calc(input);
+				});
+		assertEquals("ERROR! Could not cast input string "+"\"" + input[0] + "\" to double.", exception.getMessage());
+	}
+
+
+}
+
+class CalculatorIO{
+	public double result;
+	public String[] input;
+
+	CalculatorIO(double result, String[] input){
+		this.result = result;
+		this.input = input;
+	}
+}
---
+++ test/averkova_ebner/StackTests.java
@@ -0,0 +1,107 @@
+package averkova_ebner;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(value = Parameterized.class)
+public class StackTests {
+	private final static int stackSize = 10;
+	private Stack<String> stack;
+
+	public StackTests(String name, Stack<String> stack) {
+		this.stack = stack;
+	}
+
+	@Parameters(name = "{index}: {0}")
+	public static Collection<Object[]> data() {		
+		Object[][] data = new Object[][] { 
+			{ "StackLinkedList", new StackLinkedList<Object>(stackSize) },
+			{ "StackArray", new StackArray<Object>(stackSize) }
+		};
+		return Arrays.asList(data);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		stack = stack.getClass().getConstructor(int.class).newInstance(stackSize);
+	}
+
+	@Test
+	public void returnTrueIfStackIsEmpty() {		
+		assertEquals(true, stack.isEmpty());
+		stack.push("testItem");
+		assertEquals(false, stack.isEmpty());
+	}
+
+	@Test
+	public void popOperationOnEmptyStackShouldThrowException() {	
+		Throwable exception = Assertions.assertThrows(IndexOutOfBoundsException.class,
+				() -> {
+					stack.pop();
+				});
+		assertEquals("ERROR! Pop-operation on empty stack!", exception.getMessage());
+	}
+	
+	@Test
+	public void popShouldReturnTopElementAndRemoveIt(){
+		stack.push("1st Element");
+		stack.push("2nd Element");
+		stack.push("3rd Element");
+		assertEquals("3rd Element", stack.pop());
+		assertEquals("2nd Element", stack.pop());
+	}
+
+	@Test
+	public void peekOperationOnEmptyStackShouldThrowException() {		
+		Throwable exception = Assertions.assertThrows(IndexOutOfBoundsException.class,
+				() -> {
+					stack.peek();
+				});
+		assertEquals("ERROR! Peek-operation on empty stack!", exception.getMessage());
+	}
+	
+	@Test
+	public void peekShouldReturnTopElement(){
+		stack.push("Item 1");
+		stack.push("Item 2");
+		stack.push("Item 3");
+		assertEquals("Item 3", stack.peek());
+	}
+	
+	@Test
+	public void getSizeShouldReturnSizeOfStack() {
+		assertEquals(0, stack.getSize());
+		stack.push("testItem");
+		assertEquals(1, stack.getSize());
+	}
+	
+	@Test
+	public void pushShouldPlaceElementsOnStack() {
+		createFullStack();
+		assertEquals(stackSize, stack.getSize());
+	}
+	
+	@Test
+	public void pushElementOnFullStackShouldThrowException() {
+		createFullStack();
+		Throwable exception = Assertions.assertThrows(IndexOutOfBoundsException.class,
+				() -> {
+					stack.push("testItem");
+				});
+		assertEquals("ERROR! Push-operation on full stack! Stack size = " + stackSize, exception.getMessage());
+	}
+
+	private void createFullStack() {
+		for(int i = 0; i < stackSize; i++) {
+			stack.push("testItem");
+		}
+	}
+
+}
